3c26c6e70dfbf1c721371709c6d172d89639ecaa
Anna <anaxz@users.noreply.github.com>
2022-12-02 09:48:01 +0000

    add components

---
+++ src/components/JoinLobbyForm/index.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const JoinLobbyForm = () => {
+    return <></>
+}
+
+export default JoinLobbyForm;
---
+++ src/components/index.jsx
@@ -0,0 +1,7 @@
+export { default as AnswerBox } from './AnswerBox';
+export { default as Difficulty } from './Difficulty';
+export { default as JoinLobbyForm } from './JoinLobbyForm';
+export { default as Leaderboard } from './Leaderboard';
+export { default as Topic } from './Topic';
+export { default as UsernameForm } from './UsernameForm';
+export { default as loadingSpinner } from './loadingSpinner';
