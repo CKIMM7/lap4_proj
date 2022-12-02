0b8224a91448a07d400b79afde4313ea2e5db788
bradgichangi <bradgichangi@gmail.com>
2022-12-02 09:57:48 +0000

    Username form elements created

--- src/components/UsernameForm/index.jsx
+++ src/components/UsernameForm/index.jsx
@@ -1,7 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const UsernameForm = () => {
-    return <></>
+
+    const [ textInput, setTextInput ] = useState('')
+
+    function handleChange(e) {
+        setTextInput(e.target.value)
+    }
+
+    function handleSubmit (e) {
+        e.preventDefault()
+        console.log(e.target.value)
+    } 
+
+    return (
+        <>
+            <form onSubmit={handleSubmit}>
+                <label htmlFor="username-input">Enter Username:</label>
+                <input id="username-input" type="text" value={ textInput } onChange={ handleChange } />
+                <input type="submit" value="Enter" />
+            </form>
+        </>
+    )
 }
 
 export default UsernameForm;
