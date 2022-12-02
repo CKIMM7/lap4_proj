a5843ba6d5f241ba45b8e49224d13f4433fc1a1a
Anna <anaxz@users.noreply.github.com>
2022-12-02 19:48:26 +0000

    add buttons for topics & difficulty and their functionality

--- src/components/Difficulty/index.jsx
+++ src/components/Difficulty/index.jsx
@@ -4,7 +4,8 @@ const Diffculty = ({ level }) => {
 
     function startGame(e) { 
         e.preventDefault()
-        console.log('Go to beginner game')
+        console.log(`Goto ${e.target.value} game`)
+        setMode(e.target.value)
     }
 
     function displayLeaderboard(e) { 
@@ -12,14 +13,22 @@ const Diffculty = ({ level }) => {
         console.log('Show leaderboard')
     }
 
-    return (
-        <>
-            <div className='difficulty'>
-                <button onClick={startGame}>Beginner</button>
-                <button onClick={displayLeaderboard}>*Leaderboard Icon*</button>
-            </div>
-        </>
-    )
+    return <div className='difficulty'>
+        <div id="list-of-difficulty">
+            <button type="button" className="choose-mode" onClick={startGame} value="easy" >
+                <h2>Beginner</h2>
+            </button>
+            <button type="button" className="choose-mode" onClick={startGame} value="medium" >
+                <h2>Intermediate</h2>
+            </button>
+            <button type="button" className="choose-mode" onClick={startGame} value="hard" >
+                <h2>Expert</h2>
+            </button>
+        </div>
+
+        {/* <button onClick={startGame}>Beginner</button> */}
+        <button onClick={displayLeaderboard}>*Leaderboard Icon*</button>
+    </div>
 }
 
 export default Diffculty;
--- src/components/Topic/index.jsx
+++ src/components/Topic/index.jsx
@@ -1,14 +1,37 @@
 import React from 'react';
 
 const Topic = ({ topic }) => {
-    return (
-        <div className='topic'>
-            <h3>Topic Title</h3>
+
+    const handleCategory = (e) => {
+        const subject = e.target.value;
+        console.log('subject: '+subject)
+        
+        listOfCategory.find(s => {
+            // double check this with using redux
+            if(s.subject === subject) setCategory(s.id)
+        })
+    }
+
+    return <div id='list-of-topics'>
+        <button type="button" className='topic' onClick={handleCategory} value='history' >
+            <h3>History</h3>
             <div className='topic-info'>
                 <p>Topic Info</p>
             </div>
-        </div>
-    )
+        </button> 
+        <button type="button" className='topic' onClick={handleCategory} value='science&nature' >
+            <h3>Science & Nature</h3>
+            <div className='topic-info'>
+                <p>Topic Info</p>
+            </div>
+        </button> 
+        <button type="button" className='topic' onClick={handleCategory} value='sports' >
+            <h3>Sports</h3>
+            <div className='topic-info'>
+                <p>Topic Info</p>
+            </div>
+        </button>
+    </div>
 }
 
 export default Topic;
