690e62922374cc63ae1f2f02073a3388c8ac00f8
Anna <anaxz@users.noreply.github.com>
2022-12-05 10:31:45 +0000

    add the timer comp from staging

---
+++ src/components/Timer/index.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import { useState, useRef } from "react"
+import './index.css'
+
+const Timer = () => {
+    const [timer, setTimer] = useState(10);
+    const id = useRef(null);
+    const clear=()=>{
+    window.clearInterval(id.current)
+    }
+    useEffect(()=>{
+        id.current = window.setInterval(()=>{
+            setTimer((time)=>time-1)
+        },1000)
+        return ()=>clear();
+    },[])
+
+    useEffect(()=>{
+        if(timer===0){
+            clear()
+        }
+
+    },[timer])
+
+
+    return (
+    <div className="Timer">
+
+    <div id="timeDisplay">Time left : {timer} </div>
+
+    </div>
+    );
+}
+  
+
+export default Timer;
