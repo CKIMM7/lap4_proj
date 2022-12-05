9eb772051346be868f306d7177dc162e0272a232
bradgichangi <bradgichangi@gmail.com>
2022-12-05 12:07:50 +0000

    Routes added

--- src/App.js
+++ src/App.js
@@ -1,30 +1,12 @@
-import { useState, useEffect } from 'react';
-
+import { useState, useEffect } from 'react'
 import { Routes, Route } from 'react-router-dom'
-import useGetGames from './hooks/useGetGames';
-import { useDispatch, useSelector } from 'react-redux';
-import io from "socket.io-client";
-import useUserStatus from './hooks/useUserStatus';
-import { roomActions } from './store/roomSlice';
-import Room from './pages/room';
+import LobbyCode from './components/LobbyCode';
 
 // const socket = io.connect("https://helpful-taffy-b1fa62.netlify.app/");
 
 function App() {
 
-  const dispatch = useDispatch();
-  const { status } = useGetGames(10, 11, 'easy', 'multiple')
-
-  const { createRoom, sendMessage, setRoom, setMessage } = useUserStatus();
-
-  const userData = useSelector(state => state.user.user);
-  const roomsArray = useSelector(state => state.room.room);
-  const messageReceived = useSelector(state => state.room.messageReceived);
-
-  let createdRooms = roomsArray.map((r,i) => {
-      console.log(r)
-      return <Room data={r} key={i}></Room>
-  })
+  
 
   // useEffect(() => {
   //   console.log(roomsArray)
@@ -32,28 +14,12 @@ function App() {
   // }, [roomsArray])
 
 
-return (
-
-  <div className="App">
-      {/* <h2>user: {socket.id}</h2> */}
-      {/* <input
-        placeholder="Room Number..."
-        onChange={(event) => {
-          dispatch(roomActions.setRoom(event.target.value));
-        }}  
-      /> */}
-      <button onClick={createRoom}> Create Room</button>
-      <input
-        placeholder="Message..."
-        onChange={(event) => {
-          setMessage(event.target.value);
-        }}
-      />
-      <button onClick={sendMessage}> Send Message</button>
-      <h1> Message:</h1>
-      {messageReceived}
-      {createdRooms}
-    </div>
-)}
+  return (
+    <Routes>
+      <Route path='/' element={<h1>Welcome to the quiz</h1>} />
+      <Route path='/lobby/:id' element={<LobbyCode/> } />
+    </Routes>
+  )
+}
 
 export default App;
