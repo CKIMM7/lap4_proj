90c1abfcad715f85a9175d60a3a7747628f647fd
Anna <anaxz@users.noreply.github.com>
2022-12-07 12:25:30 +0000

    change settings

--- server/dbconfig.js
+++ server/dbconfig.js
@@ -1,8 +1,8 @@
 const express = require('express');
-
+const env = './.env'
 const { Client } = require('pg')
 
-const client = new Client('postgresql://bradley:99DLsT1U7bF2UCDFvKlEyg@basic-yak-3859.6zw.cockroachlabs.cloud:26257/quizdb?sslmode=verify-full')
+const client = new Client(env)
 
 client.connect()
 
