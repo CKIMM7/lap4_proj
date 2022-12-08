97670dc2587816ec732dd66297c5b829e4d8853a
Arqestra <mshawdietrich@gmail.com>
2022-12-08 12:34:20 +0000

    Added styling to the categories page

--- src/components/Category/index.jsx
+++ src/components/Category/index.jsx
@@ -2,13 +2,14 @@ import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import useGetGames from "../../hooks/useGetGames"
 import { usersActions } from '../../store/usersSlice';
+import Rockets from '../Rockets'
 
 import './Category.css'
 
 const listOfCategory = [
-    {id: 23, subject: 'history', desc: 'history desc...' },
-    {id: 17, subject: 'science&nature', desc: 'sci & nature desc...' },
-    {id: 21, subject: 'sports', desc: 'sports desc...' }
+    {id: 23, subject: 'history', desc: 'Think you can handle the past? Choose history to see how much you know about the lifetime of our planet.' },
+    {id: 17, subject: 'science&nature', desc: 'Do you know what the mass of the Sun is? Or perhaps the role of the mitochondria? Make sure you are ready to be tested!' },
+    {id: 21, subject: 'sports', desc: 'I think we all know the fast 100m time run by none other than Usain Bolt. But can you tell me length of a standard football field?' }
 ]
 
 const Category = ({ }) => {
@@ -33,29 +34,37 @@ const Category = ({ }) => {
         })
     }
 
-    return <div id='list-of-categorys'>
-        <button type="button" className='category' onClick={(e) => handleCategory(e, 'history')} 
-        id='history' value={category} >
-            <h3>History</h3> 
-            <div className='category-info'>
-                <p>{ listOfCategory[0].desc }</p>
+    return ( 
+        <div className="pagecover">
+            <div id='listofcategories'>
+                <div className="catcont" id="historycont">
+                    <h3 className='cattitle'>History</h3> 
+                    <div className='category-info'>
+                        <p className="catdesc">{ listOfCategory[0].desc }</p>
+                    </div>
+                    <button type="button" className='nes-btn is-primary' onClick={(e) => handleCategory(e, 'history')} 
+                    id='history' value={category} >Select This Category</button>
+                </div> 
+                <div className="catcont" id="scicont">
+                    <h3 className='cattitle'>Science & Nature</h3>
+                    <div className='category-info'>
+                        <p className="catdesc">{ listOfCategory[1].desc }</p>
+                    </div>
+                    <button type="button" className='nes-btn is-primary' onClick={(e) => handleCategory(e, 'science&nature')} 
+                    id='science&nature' value={category} >Select This Category</button> 
+                </div>
+                <div className="catcont" id="sportcont">
+                    <h3 className='cattitle'>Sports</h3>
+                    <div className='category-info'>
+                        <p className="catdesc">{ listOfCategory[2].desc }</p>
+                    </div>
+                    <button type="button" className='nes-btn is-primary' onClick={(e) => handleCategory(e, 'sports')} 
+                    id='sports' value={category} >Select This Category</button>
+                </div>
             </div>
-        </button> 
-        <button type="button" className='category' onClick={(e) => handleCategory(e, 'science&nature')} 
-        id='science&nature' value={category} >
-            <h3>Science & Nature</h3>
-            <div className='category-info'>
-            <p>{ listOfCategory[1].desc }</p>
-            </div>
-        </button> 
-        <button type="button" className='category' onClick={(e) => handleCategory(e, 'sports')} 
-        id='sports' value={category} >
-            <h3>Sports</h3>
-            <div className='category-info'>
-            <p>{ listOfCategory[2].desc }</p>
-            </div>
-        </button>
-    </div>
+            <Rockets />
+        </div>
+    )
 }
 
 export default Category;
