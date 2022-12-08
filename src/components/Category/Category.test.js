7cfd1596e799bc46a5f63209ed1164fc1510e77a
Anna <anaxz@users.noreply.github.com>
2022-12-08 20:08:00 +0000

    save current tests

--- src/components/Category/Category.test.js
+++ src/components/Category/Category.test.js
@@ -28,37 +28,92 @@ describe('Category', () => {
         render(<MockCategory/>)
     })
 
+    test("Pick a Category!", () => {
+        // const btnElements = screen.getAllByRole('button');
+        const element = screen.getByRole('heading', { name: /Pick a Category!/i })
+        expect(element).toBeTruthy();
+    }) 
+
+    test("Test your wit with three categories to choose from.", () => {
+        // const btnElements = screen.getAllByRole('button');
+        const element = screen.getByRole('heading', { name: /Test your wit with three categories to choose from./i })
+        expect(element).toBeTruthy();
+    }) 
+
     test('all 3 categories are displayed', () => {
         // const btnElements = screen.getAllByRole('button');
         const elements = screen.getAllByRole('heading')
         expect(elements).toBeTruthy();
     }) 
 
-    test('one topic has been clicked', () => {
-        const btnElement = screen.getByRole('heading', { name: /History/i });
-        fireEvent.click(btnElement)
-
-        btnElement.id = mockOneCategory.subject;
-        expect(btnElement.id).toBe('history')
-    })
-
-    test('updateInput sets the category selected successfully', () => {
-        const btnElement = screen.getByRole('heading', { name: /History/i });
-        fireEvent.click(btnElement)
-
-        btnElement.value = mockOneCategory.id;
-        expect(btnElement.value).toBe(23)
-    })
-    
     test('display categories', () => {
         const divElement = screen.findByTestId(`list-of-categories`)
         expect(divElement).toBeTruthy();
-
     })
 
     test('display heading for categories', () => {
         const divElement = screen.findByTestId(`choosecategory`)
         expect(divElement).toBeTruthy();
-
     })
+
+    test('there should be 3 buttons', () => {
+        const btns = screen.getAllByRole('button')
+        expect(btn).toBeTruthy(); 
+    })
+
+    describe('history category', () => {
+        test('the topic has been clicked', () => {
+            const btnElement = screen.getByRole('heading', { name: /History/i });
+            fireEvent.click(btnElement)
+    
+            btnElement.id = mockOneCategory.subject;
+            expect(btnElement.id).toBe('history')
+        })
+    
+        test('updateInput sets the category selected successfully', () => {
+            const btnElement = screen.getByRole('heading', { name: /History/i });
+            fireEvent.click(btnElement)
+    
+            btnElement.value = mockOneCategory.id;
+            expect(btnElement.value).toBe(23)
+        })
+    }) 
+
+    describe('Science & Nature category', () => {
+        test('the topic has been clicked', () => {
+            const btnElement = screen.getByRole('heading', { name: /Science & Nature/i });
+            fireEvent.click(btnElement)
+    
+            btnElement.id = mockOneCategory.subject;
+            expect(btnElement.id).toBe('science')
+        })
+    
+        test('updateInput sets the category selected successfully', () => {
+            const btnElement = screen.getByRole('heading', { name: /Science & Nature/i });
+            fireEvent.click(btnElement)
+    
+            btnElement.value = mockOneCategory.id;
+            expect(btnElement.value).toBe(17)
+        })
+    }) 
+    
+    describe('sports category', () => {
+        test('the topic has been clicked', () => {
+            const btnElement = screen.getByRole('heading', { name: /Sports/i });
+            fireEvent.click(btnElement)
+    
+            btnElement.id = mockOneCategory.subject;
+            expect(btnElement.id).toBe('science')
+        })
+    
+        test('updateInput sets the category selected successfully', () => {
+            const btnElement = screen.getByRole('heading', { name: /Sports/i });
+            fireEvent.click(btnElement)
+    
+            btnElement.value = mockOneCategory.id;
+            expect(btnElement.value).toBe(21)
+        })
+    }) 
+
+
 })
